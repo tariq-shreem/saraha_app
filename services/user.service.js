fd77d612f4eb59755533545fd9c0601fdc521683
tariq-shreem <tariqshreem00@gmail.com>
2022-10-30 19:51:21 +0200

    r

--- services/user.service.js
+++ services/user.service.js
@@ -30,7 +30,7 @@ module.exports.signIn = async ( req,res)=>{
 
                 
                     
-                    res.json({message:'login',token});
+                    res.json({message:'login',user});
                 
                 
         }else {
